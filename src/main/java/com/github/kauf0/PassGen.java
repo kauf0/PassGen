1509a49da0730add0902002ea012c908c3d1532f
kauf0 <101307532+kauf0@users.noreply.github.com>
2024-12-15 19:14:26 +0900

    Created them repo
    Blank project with some initial planning

---
+++ src/main/java/com/github/kauf0/PassGen.java
@@ -0,0 +1,14 @@
+package com.github.kauf0;
+
+public class PassGen {
+    public static void main(String[] args) {
+        // This program's purpose is to generate random passwords based on the user defined settings
+        // Such as length and complexity.
+
+        // For this everything will supposedly be put inside loop, make it listen for length and amount ints
+        // + make multiple choice for complexity (I'm thinking of some sort of linux-like util that they use there to check
+        // them password complexity.
+        
+        // I also would like to implement some TUI here (and if I do, then make the same for CalcCli)
+    }
+}
